dbeb9e410737d52c6339e5f9393ef12e82262c31
Marco Di Pillo Tomic <marcodpt@gmail.com>
2018-09-04 20:33:53 -0300

    First commit

---
+++ src/app.vue
@@ -0,0 +1,45 @@
+<script type="text/babel">
+  import Vue from '../node_modules/vue/dist/vue.js'
+  import inputag from './index.vue'
+
+  new Vue({
+    components: {
+      'vue-inputag': inputag
+    },
+    data: {
+      model: {
+        number: 3.14,
+        integer: 25,
+        date: '2018-01-01',
+        string: 'Fork me on github',
+        text: 'This is a sample text\nEdit as many times as you want',
+        scientist: 'Gauss',
+        country: 2,
+        file: null
+      },
+      github: 'https://github.com/marcodpt/vue-inputag',
+      options: [
+        "Eistein",
+        "Newton",
+        "Gauss",
+        "Euler",
+        "Riemann"
+      ],
+      options2: [
+        {
+          id: 1,
+          label: 'Italy'
+        }, {
+          id: 2,
+          label: 'France'
+        }, {
+          id: 3,
+          label: 'Germany'
+        }, {
+          id: 4,
+          label: 'England'
+        }
+      ]
+    }
+  }).$mount('#app')
+</script>
